84b9acb3b022dccf3e6b68863d3b071c8ae4c644
mariagrigoratou <43170280+mariagrigoratou@users.noreply.github.com>
2021-03-06 11:13:14 +0100

    Update thermal_plot_phytogroups.py

--- code/thermal_plot_phytogroups.py
+++ code/thermal_plot_phytogroups.py
@@ -12,7 +12,7 @@ from matplotlib import style
 import seaborn as sns 
 
 
-df = pd.read_csv('E:PHD\FORAMECOGENIE_MARCH_2020\EXCEL_METAANALYSIS\FUTURE\RCP\\thermal_plot.csv')
+df = pd.read_csv('thermal_plot.csv')
 df.shape
 df.dtypes
 
